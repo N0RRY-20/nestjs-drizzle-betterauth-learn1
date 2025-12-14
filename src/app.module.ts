f25e3991f013ef842ec4ea737c6f9118352e916d
N0RRY-20 <orywu1994@gmail.com>
2025-12-15 00:51:07 +0700

    feat: Menambahkan inisialisasi aplikasi NestJS, skema database autentikasi, dan integrasi modul autentikasi.

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DatabaseModule } from './database/database.module';
+import { AuthModule } from '@thallesp/nestjs-better-auth';
+import { auth } from './lib/auth';
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [DatabaseModule, AuthModule.forRoot({ auth })],
   controllers: [AppController],
   providers: [AppService],
 })
